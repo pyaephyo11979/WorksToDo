27c53a352a675ab541c41b70d536b30d55e56ab9
pyaephyo11979 <www.pyaephyoehlaing16@gmail.com>
2023-10-12 21:14:56 +0630

    Update

--- script.js
+++ script.js
@@ -47,6 +47,27 @@ input.addEventListener('keydown',(e)=>{
         input.value="";
     }
 })
+let clock=document.getElementById('clockSpace');
+function showTime(){
+    let date=new Date();
+let hour=date.getHours();
+let min=date.getMinutes();
+let sec=date.getSeconds();
+let session="AM";
+if(hour==0){
+    hour=12;
+}
+if(hour>12){
+    hour=hour-12;
+    session="PM";
+}
+hour=(hour<10)? "0" + hour: hour;
+min=(min<10)? "0" + min: min;
+sec=(sec<10)? "0" + sec: sec;
+clock.innerHTML=`${hour}:${min}:${sec} ${session}`
+setTimeout(showTime,1000);
+}
+showTime();
 // restoreBtn.addEventListener('click',()=>{
 //     let todolist=JSON.parse(localStorage.getItem('todos'));
 //     let li=document.createElement('li');
